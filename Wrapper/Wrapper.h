b38791a6bee4957a016c9f49a18f85fb6aa70694
Jonny Holland <jonholland@icloud.com>
2021-07-15 16:19:22 -0400

    Initial Commit

---
+++ Wrapper/Wrapper.h
@@ -0,0 +1,18 @@
+//
+//  Wrapper.h
+//  Wrapper
+//
+//  Created by Jonathan Holland on 7/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Wrapper.
+FOUNDATION_EXPORT double WrapperVersionNumber;
+
+//! Project version string for Wrapper.
+FOUNDATION_EXPORT const unsigned char WrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wrapper/PublicHeader.h>
+
+
